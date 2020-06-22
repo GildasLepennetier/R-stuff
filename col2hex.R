9a3bea3c0ce716c4323cda7c3848bbb8e9e13caf
Gildas Lepennetier <gildas.lepennetier@hotmail.fr>
2020-06-22 11:46:11 +0200

    first commit

---
+++ col2hex.R
@@ -0,0 +1,5 @@
+#### use source("/PATH/") #to load those functions
+col2hex=function(col,alpha=NULL,warn=T){ 
+# Function to convert for colour name like "blue" to hex colors, with possibility to add alpha chanel 
+# This function is NOT a clone of: https://rdrr.io/cran/gplots/man/col2hex.html, but carefull since it is the same name.
+if(!is.null(alpha)&warn){warning("Carefull, alpha chanel activated, hex will be 9-char length instead of 7.")};return(rgb(col2rgb(col)[1,]/255,col2rgb(col)[2,]/255,col2rgb(col)[3,]/255,alpha=alpha ))}
---
+++ correct_dist_matrix_hamming.R
@@ -0,0 +1,16 @@
+
+correct_dist_matrix_hamming=function(dist_mat, replace="auto" ){
+	# This function take a distance matrix using hamming distance and avoid having Infinite distances because of impossible comparison 
+	# This happens when the sequeces does not have the same length.
+	# Exemple: require(stringdist) ; d_hamming = stringdistmatrix(a = c("AA","AAA","AAB"), b = c("AA","AAA","AAB"), method = "hamming",useNames ="strings") #edit distance
+	if(replace=="auto"){
+		replace = max( dist_mat[dist_mat!=max(dist_mat)]) + 1 # avoid having giant distances
+	}
+	if(any(dist_mat==Inf)){
+		print(paste("Infinite detected in dist matrix (hamming?), will crash the hclust, replacing by",replace))
+		dist_mat[ dist_mat==Inf ] <- replace
+	}
+	return(dist_mat)
+}
+
+
