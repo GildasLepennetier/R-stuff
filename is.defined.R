74546edb37846d023e608ec9514782631148f2be
Lepennetier <GildasLepennetier@users.noreply.github.com>
2020-09-21 12:22:07 +0200

    Update is.defined.R

--- is.defined.R
+++ is.defined.R
@@ -1,4 +1,4 @@
-#### use source("/PATH/") #to load this function
+#### use source("https://raw.githubusercontent.com/GildasLepennetier/R-stuff/master/is.defined.R") #to load this function
 is.defined <- function(sym) {
 	# find if an object exists, e.g. variable
 	sym <- deparse(substitute(sym))
