aaa5e2ba941f83eb1b007ff74a1b4f83af888553
Lepennetier <GildasLepennetier@users.noreply.github.com>
2020-09-21 12:24:37 +0200

    Update right_align.R

--- right_align.R
+++ right_align.R
@@ -1,3 +1,4 @@
+# use source("https://raw.githubusercontent.com/GildasLepennetier/R-stuff/master/right_align.R") to load this function
 right_align=function(x, space="."){
 	"This function take a vector as input and add spacer (default: .) on the left to left-align characters"
 	MAX=max(nchar(x))
