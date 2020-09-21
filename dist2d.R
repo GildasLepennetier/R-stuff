bd90f78f5dba8ea897b7b4d6f72797fb3a0d36f4
Lepennetier <GildasLepennetier@users.noreply.github.com>
2020-09-21 12:23:36 +0200

    Update dist2d.R

--- dist2d.R
+++ dist2d.R
@@ -1,3 +1,5 @@
 #### use source("https://raw.githubusercontent.com/GildasLepennetier/R-stuff/master/dist2d.R") #to load this functions
-#this calculate the distance between two points in 2d
-dist2d = function(x1,y1,x2,y2){return(sqrt( (x2-x1)^2 + (y2-y1)^2 ))}
+dist2d = function(x1,y1,x2,y2){
+  #this function calculate the distance between two points in 2d (x1 y1 -> x2 y2)
+  return(sqrt( (x2-x1)^2 + (y2-y1)^2 ))
+}
