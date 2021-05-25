f996cd3e83f5b6d49e4131e52ab09728e14190d9
Lepennetier <GildasLepennetier@users.noreply.github.com>
2021-05-25 16:02:48 +0200

    Create paste2.R

---
+++ paste2.R
@@ -0,0 +1,8 @@
+paste2 <- function(...,sep=",") {
+	"""This is a better paste function that deal with NA"""
+	L <- list(...)
+	L <- lapply(L,function(x) {x[is.na(x)] <- ""; x})
+	ret <-gsub(paste0("(^",sep,"|",sep,"$)"),"",gsub(paste0(sep,sep),sep,do.call(paste,c(L,list(sep=sep)))))
+	is.na(ret) <- ret==""
+	ret
+}
