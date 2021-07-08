f2918347a8bb2e7f2cfdcc8099487682d30a75ac
Lepennetier <GildasLepennetier@users.noreply.github.com>
2021-07-08 15:56:06 +0200

    Update SignStar.R

--- SignStar.R
+++ SignStar.R
@@ -4,8 +4,8 @@
 # Add stars if the value is < cuttoff element 1. Add more stars if the value is < cuttof 2.
 # There will be a space between the values and the stars (number of significant values constroled by option Signif)
 # Do not ocnvert to scientific format by default (option Scienti = F)
-SignStar<-function(x,cutoff=c(0.05,0.01,0.001), Signif=4, Scienti=F){
+SignStar<-function(x,cutoff=c(0.05,0.01,0.001), Signif=4, Scienti=F, Star="*"){
 	STR<-paste0(as.character( format(signif(x,Signif), scientific = Scienti) )," ") #R auto convert to scientific format
-	for(lim in cutoff){STR = paste0(STR, ifelse(x<lim,yes="*",no="") ) }
+	for(lim in cutoff){STR = paste0(STR, ifelse(x<lim,yes=Star,no="") ) }
 	return(gsub(" $","",STR))
 }
